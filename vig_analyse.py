92bc432eb7ffec9580db9ed0083fa5a7deed0a43
User with sudo ability <moadouani@base-debian10-9>
2022-02-06 14:16:18 +0100

    forst work

---
+++ vig_analyse.py
@@ -0,0 +1,26 @@
+from cgitb import text
+from vig import *
+
+
+def get_occurences(texte):
+    occ
+    for in range(0,len(texte))
+
+
+
+
+def freq(texte):
+    d = {}
+    texte = del_space(to_miniscule(texte))
+    occurences = []
+    
+    for c in occurences:
+        if c not in d:
+            if c not in car:
+                d[c.lower()] = 1
+        else:
+            d[c.lower()] += 1
+            
+    return d
+
+
